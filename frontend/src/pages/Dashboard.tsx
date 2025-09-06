9aa682cf1f62626cca8097e68e41b027e854e90f
Pify <rabbid89lolz@gmail.com>
2025-09-06 22:27:28 +0700

    refactor: move static assets to public folder and update image paths

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -2,7 +2,7 @@ function Dashboard() {
     return (
         <div className="container">
             <div>
-                <img src="src/assets/placeholder.png" alt="placeholder img" style={{ width: "70%" }} />
+                <img src="/assets/placeholder.png" alt="placeholder img" style={{ width: "70%" }} />
                 <label className="fs-2">Welcome to Maktabah Online</label>
             </div>
         </div>
